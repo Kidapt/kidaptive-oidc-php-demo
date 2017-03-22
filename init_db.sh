6c907be2d27e1788911425177f32702a941954cf
Solomon Liu <sliu198@gmail.com>
2017-03-22 16:59:54 -0700

    change name and format of clientInfo file

--- init_db.sh
+++ init_db.sh
@@ -12,9 +12,9 @@ require_once "server.php";
 $clientId = base64_encode(random_bytes(9));
 $clientSecret  = base64_encode(random_bytes(33));
 $storage->setClientDetails($clientId, $clientSecret, "https://develop.kidaptive.com/v3/openid/user/ajax-cb", "authorization_code refresh_token", "openid offline_access");
-echo "client_id: ".$clientId."\n";
-echo "client_secret: ".$clientSecret;
-' > clientInfo &&
+echo "client_id=".$clientId."\n";
+echo "client_secret=".$clientSecret."\n";
+' > clientInfo.properties &&
 
 #generate rsa key pair
 php -r '
